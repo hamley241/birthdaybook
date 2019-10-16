488fbef5602aed1879c4e6f3e39bcaf31382c0d4
hamley241 <gpatley@gmail.com>
2019-10-16 14:41:55 -0400

    Adding login

--- birthdaybook/birthdays/views.py
+++ birthdaybook/birthdays/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
 
+from django.contrib.auth import authenticate, login
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        do_something_else()
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponse("Hello, world. You're at the polls index.")
+    else:
+        return HttpResponse("Login failed")
+
