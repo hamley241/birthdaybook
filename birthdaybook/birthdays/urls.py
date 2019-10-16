1eae7fcb4c792eb475c8f6e6264d65c59e147480
hamley241 <gpatley@gmail.com>
2019-10-16 17:27:06 -0400

    Bootsrap check - all apis check

--- birthdaybook/birthdays/urls.py
+++ birthdaybook/birthdays/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('delete/<int:birthday_id>/', views.delete, name='delete'),
+    path('<int:birthday_id>/', views.detail, name='detail'),
     path('<int:birthday_id>/update', views.update, name='update'),
     path('update/', views.detail, name='update1'),
     path('add/', views.add, name='add'),
